06d77505597c02b991178fda9e5b93ee6d67ceff
raishivamm <shrai0432@gmail.com>
2023-11-07 23:58:52 +0530

    Episode 2

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I m an h1 tag"),
+    React.createElement("h1", {}, "It is Namastey React"),
     React.createElement("h2", {}, "I m an h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I m an h1 tag"),
+    React.createElement("h1", {}, "I m an h4 tag"),
     React.createElement("h2", {}, "I m an h2 tag"),
   ]),
 ]);
---
+++ README.md
@@ -0,0 +1,1 @@
+I am Learning React from Namastey React by Akshay Saini
